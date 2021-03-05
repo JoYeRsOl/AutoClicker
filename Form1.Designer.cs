0c9e07c571becbb17189fac4b67be2c6ca7739f6
joyersol <joyer1339@gmail.com>
2021-03-05 19:43:36 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace AutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clicktimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clicktimer
+            // 
+            this.clicktimer.Interval = 1000;
+            this.clicktimer.Tick += new System.EventHandler(this.Clicktimer_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 60);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Click interval";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(151, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "milliseconds";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(96, 28);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(49, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "100";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 28);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(49, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "secs";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(12, 77);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(240, 60);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Click duration";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "milliseconds";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(49, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "100";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(49, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "secs";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start (F1)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop (F2)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(264, 189);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AutoClicker";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer clicktimer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace AutoClicker
+{
+    public partial class Form1 : Form
+    {
+        const int MOUSEEVENTF_LEFTDOWN = 2;
+        const int MOUSEEVENTF_LEFTUP = 4;
+        const int MOUSEEVENTF_RIGHTDOWN = 8;
+        const int MOUSEEVENTF_RIGHTUP = 16;
+        const int MOUSEEVENTF_MIDDLEUP = 32;
+        const int MOUSEEVENTF_MIDDLEDOWN = 64;
+        public const int START_HOTKEY = 1;
+        public const int STOP_HOTKEY = 2;
+        //input type constant
+        const int INPUT_MOUSE = 0;
+        private int Duration = 1000;
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        public struct INPUT
+        {
+            public uint type;
+            public MOUSEINPUT mi;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MousePoint
+        {
+            public int X;
+            public int Y;
+
+            public MousePoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+        public static MousePoint GetCursorPosition()
+        {
+            var gotPoint = GetCursorPos(out MousePoint currentMousePoint);
+            if (!gotPoint) { currentMousePoint = new MousePoint(0, 0); }
+            return currentMousePoint;
+        }
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern int SendInput(int nInputs, ref INPUT pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        void ClickAtCurrentPosition(int duration)
+        {
+            if (duration < 0)
+            {
+                MessageBox.Show("Bad click duration");
+                return;
+            }
+            else
+            {
+            }
+
+            MousePoint mousePoint = GetCursorPosition();
+            INPUT i = new INPUT
+            {
+                type = INPUT_MOUSE
+            };
+            i.mi.dx = mousePoint.X;
+            i.mi.dy = mousePoint.Y;
+            i.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            i.mi.dwExtraInfo = IntPtr.Zero;
+            i.mi.mouseData = 0;
+            i.mi.time = 0;
+
+            SendInput(1, ref i, Marshal.SizeOf(i));
+
+            if(duration != 0)
+            {
+                Thread.Sleep(duration);
+            }
+
+            mousePoint = GetCursorPosition();
+            i.mi.dx = mousePoint.X;
+            i.mi.dy = mousePoint.Y;
+            i.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            SendInput(1, ref i, Marshal.SizeOf(i));
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            RegisterHotKey(this.Handle, START_HOTKEY, 0, 112);
+            RegisterHotKey(this.Handle, STOP_HOTKEY, 0, 113);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == START_HOTKEY)
+            {
+                Start();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == STOP_HOTKEY)
+            {
+                Stop();
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Clicktimer_Tick(object sender, EventArgs e)
+        {
+            ClickAtCurrentPosition(Duration);
+        }
+
+        private void Start()
+        {
+            if (clicktimer.Enabled)
+            {
+                MessageBox.Show("Autoclicker already running", "Already running");
+            }
+            else
+            {
+                bool intervalError = false;
+                bool durationError = false;
+                int interval = 1000;
+                try
+                {
+                    interval = 1000 * Convert.ToInt32(textBox3.Text) + Convert.ToInt32(textBox4.Text);
+                }
+                catch
+                {
+                    intervalError = true;
+                }
+                try
+                {
+                    Duration = 1000 * Convert.ToInt32(textBox2.Text) + Convert.ToInt32(textBox1.Text);
+                }
+                catch
+                {
+                    durationError = true;
+                }
+                if (!intervalError)
+                {
+                    if (!durationError)
+                    {
+                        groupBox1.Enabled = false;
+                        groupBox3.Enabled = false;
+                        clicktimer.Interval = interval;
+                        clicktimer.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid duration time enetered", "Invalid duration");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid time interval enetered", "Invalid interval");
+                }
+            }
+        }
+
+        private void Stop()
+        {
+            if (!clicktimer.Enabled)
+            {
+                MessageBox.Show("AutoClicker is not running", "Not running");
+            }
+            else 
+            {
+
+                groupBox1.Enabled = true;
+                groupBox3.Enabled = true;
+                clicktimer.Stop();
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (GetAsyncKeyState(0x70) != 0)
+            {
+                if(clicktimer.Enabled) Stop(); else Start();
+            }    
+        }
+    }
+}
